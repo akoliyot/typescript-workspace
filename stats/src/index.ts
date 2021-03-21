013e31fc345c664d299c0076ddbd25b2a421bdb8
Aswin Koliyot <akoliyot@gmail.com>
2021-03-21 12:53:20 +0100

    Add support for HTML report

--- stats/src/index.ts
+++ stats/src/index.ts
@@ -2,6 +2,7 @@ import { WinsAnalysis } from "./analyzers/WinsAnalysis";
 import { CSVFileReader } from "./CSVFileReader";
 import { MatchReader } from "./MatchReader";
 import { ConsoleReport } from "./reportTargets/ConsoleReport";
+import { HTMLReport } from "./reportTargets/HTMLReport";
 import { Summary } from "./Summary";
 
 const reader = new CSVFileReader("football.csv");
@@ -9,8 +10,5 @@ const reader = new CSVFileReader("football.csv");
 const matchReader = new MatchReader(reader);
 matchReader.load();
 
-const summary = new Summary(
-  new WinsAnalysis("Man United"),
-  new ConsoleReport()
-);
+const summary = new Summary(new WinsAnalysis("Man United"), new HTMLReport());
 summary.buildAndPrintReport(matchReader.matches);
