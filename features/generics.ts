6d7eb00afa311e043a5239665750bcbfc3d4e569
Aswin Koliyot <akoliyot@gmail.com>
2021-03-21 13:22:21 +0100

    Setup features file for generics

---
+++ features/generics.ts
@@ -0,0 +1,15 @@
+class ArrayOfNumber {
+  constructor(public collection: number[]) {}
+
+  get(index: number): number {
+    return this.collection[index];
+  }
+}
+
+class ArrayOfString {
+  constructor(public collection: string[]) {}
+
+  get(index: number): string {
+    return this.collection[index];
+  }
+}
