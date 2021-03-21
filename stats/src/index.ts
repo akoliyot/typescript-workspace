0b613b5e0b250c8570f4b69c100265afc92fb2d9
Aswin Koliyot <akoliyot@gmail.com>
2021-03-21 09:50:44 +0100

    Create CSVFileReader class

--- stats/src/index.ts
+++ stats/src/index.ts
@@ -1,16 +1,12 @@
-import fs from 'fs';
+import { CSVFileReader } from './CSVFileReader';
 import { MatchResult } from './MatchResult';
 
-const matches = fs.readFileSync('football.csv', {
-  encoding: 'utf-8'
-}).split('\n')
-  .map((row: string): string[] => {
-    return row.split(',')
-  })
+const reader = new CSVFileReader('football.csv');
+reader.read();
 
 let manUnitedWins = 0;
 
-for (let match of matches) {
+for (let match of reader.data) {
   if (match[1] === 'Man United' && match[5] === MatchResult.HomeWin) {
     manUnitedWins++;
   } else if (match[2] === 'Man United' && match[5] === MatchResult.AwayWin) {
